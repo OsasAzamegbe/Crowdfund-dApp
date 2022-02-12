d24047392f95550d0d4e426bf1d6828f17c91719
Osamudiamen <osas.azamegbe@gmail.com>
2022-02-12 14:05:05 +0000

    add compile script

---
+++ crowdfund-solidity/compile.js
@@ -0,0 +1,26 @@
+const solc = require('solc');
+const path = require('path');
+const fs = require('fs');
+
+
+const crowdfundPath = path.resolve(__dirname, 'contracts', 'Crowdfund.sol');
+const sourceCode = fs.readFileSync(crowdfundPath, 'utf-8');
+const compilerInput = {
+    language: 'Solidity',
+    sources: {
+        'Crowdfund.sol': {
+            content: sourceCode,
+        },
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['*'],
+            },
+        },
+    },
+};
+
+const compiledCode = JSON.parse(solc.compile(JSON.stringify(compilerInput)));
+
+module.exports = compiledCode.contracts['Crowdfund.sol'].Crowdfund;
