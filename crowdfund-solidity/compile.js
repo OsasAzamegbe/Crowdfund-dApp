c27f988d1790fcb949888cfa2b85b188434f3c24
Osamudiamen <osas.azamegbe@gmail.com>
2022-02-12 14:21:41 +0000

    format code

--- crowdfund-solidity/compile.js
+++ crowdfund-solidity/compile.js
@@ -1,26 +1,25 @@
-const solc = require('solc')
-const path = require('path')
-const fs = require('fs')
+const solc = require('solc');
+const path = require('path');
+const fs = require('fs');
 
-const crowdfundPath = path.resolve(__dirname, 'contracts', 'Crowdfund.sol')
-const sourceCode = fs.readFileSync(crowdfundPath, 'utf-8')
+const crowdfundPath = path.resolve(__dirname, 'contracts', 'Crowdfund.sol');
+const sourceCode = fs.readFileSync(crowdfundPath, 'utf-8');
 const compilerInput = {
-  language: 'Solidity',
-  sources: {
-    'Crowdfund.sol': {
-      content: sourceCode
+    language: 'Solidity',
+    sources: {
+        'Crowdfund.sol': {
+            content: sourceCode
+        }
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['*']
+            }
+        }
     }
-  },
-  settings: {
-    outputSelection: {
-      '*': {
-        '*': ['*']
-      }
-    }
-  }
-}
+};
 
-const compiledCode = JSON.parse(solc.compile(JSON.stringify(compilerInput)))
+const compiledCode = JSON.parse(solc.compile(JSON.stringify(compilerInput)));
 
-module.exports = compiledCode.contracts['Crowdfund.sol'].Crowdfund
-ß
+module.exports = compiledCode.contracts['Crowdfund.sol'].Crowdfund;
