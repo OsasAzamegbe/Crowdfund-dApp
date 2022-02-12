511c8aaafb1a999e5e0d0a15f0508c15edd806dd
Osamudiamen <osas.azamegbe@gmail.com>
2022-02-12 14:22:19 +0000

    add deploy script

---
+++ crowdfund-solidity/deploy.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const HdWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { abi, evm } = require('./compile');
+
+const deploy = async () => {
+    const accountProvider = new HdWalletProvider(
+        process.env.ACCOUNT_MNEMONIC,
+        process.env.INFURA_RINKEBY_ENDPOINT
+    );
+    const web3 = new Web3(accountProvider);
+
+    console.log('Getting accounts from provider...');
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Deploying contract with ABI:', JSON.stringify(abi));
+    console.log('Using account:', accounts[0]);
+    const deployedContract = await web3.eth
+        .Contract(abi)
+        .deploy({ data: evm.bytecode.object })
+        .send({ from: accounts[0], gas: '1000000' });
+
+    console.log('Deployed contract to address:', deployedContract.options.address);
+    accountProvider.engine.stop();
+}
+
+deploy();
