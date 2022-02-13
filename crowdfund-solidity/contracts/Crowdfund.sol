bf7fa7a7067e89ab3cc905d9436ad7c828f03c31
Osamudiamen <osas.azamegbe@gmail.com>
2022-02-13 21:20:08 +0000

    split Crowdfund into two contracts

--- crowdfund-solidity/contracts/Crowdfund.sol
+++ crowdfund-solidity/contracts/Crowdfund.sol
@@ -1,7 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
+
 contract Crowdfund {
+    address[] public campaigns;
+
+    function createCampaign(uint minContribution, uint targetAmount) public {
+        Campaign campaign = new Campaign(msg.sender, minContribution, targetAmount);
+        campaigns.push(address(campaign));
+    }
+
+    function getCampaigns() public view returns(address[] memory) {
+        return campaigns;
+    }
+}
+
+contract Campaign {
+
     struct Contributor {
         bool isApprover;
         uint contribution;
@@ -16,69 +31,46 @@ contract Crowdfund {
         bool isCompleted;
     }
 
-    struct Campaign {
-        address manager;
-        uint minContributionForApprover;
-        uint balance;
-        uint target;
-        uint numRequests;
-        uint numApprovers;
-        CampaignRequest[] requests;
-        mapping(address => Contributor) contributors;
-    }
+    address public manager;
+    uint public minContributionForApprover;
+    uint public target;
+    uint public numRequests;
+    uint public numApprovers;
+    CampaignRequest[] public requests;
+    mapping(address => Contributor) public contributors;
+    event RequestIdEvent(uint requestId);
 
-    uint numCampaigns;
-    mapping(uint => Campaign) public campaigns;
-    event CampaignIdEvent(uint campaignId);
-    event CampaignRequestIdEvent(uint requestId);
-
-
-    modifier validCampaign(uint campaignId) {
-        require(campaignId <= numCampaigns);
+    modifier validRequest(uint requestId) {
+        require(requestId < numRequests);
         _;
     }
 
-    modifier validRequest(uint campaignId, uint requestId) {
-        require(campaignId <= numCampaigns);
-        require(requestId <= campaigns[campaignId].numRequests);
+    modifier admin() {
+        require(manager == msg.sender);
         _;
-    }
-
-    constructor() {
-        numCampaigns = 0;
     }
     
-    function createCampaign(uint minContribution, uint target) public {
-        Campaign storage c = campaigns[numCampaigns];
-
-        c.manager = msg.sender; 
-        c.minContributionForApprover = minContribution;
-        c.balance = 0;
-        c.target = target;
-        c.numApprovers = 0;
-        c.numRequests = 0;
-
-        emit CampaignIdEvent(numCampaigns);
-        numCampaigns++;
+    constructor(address managerAddress, uint minContribution, uint targetAmount) {
+        manager = managerAddress; 
+        minContributionForApprover = minContribution;
+        target = targetAmount;
+        numApprovers = 0;
+        numRequests = 0;
     }
 
-    function contributeToCampaign(uint campaignId) validCampaign(campaignId) public payable {
-        Campaign storage campaign = campaigns[campaignId];
-        Contributor storage contributor = campaign.contributors[msg.sender];
+    function contributeToCampaign() public payable {
+        Contributor storage contributor = contributors[msg.sender];
         bool isNewApprover = !contributor.isApprover;
 
         contributor.contribution += msg.value;
-        contributor.isApprover = contributor.contribution >= campaign.minContributionForApprover;
-        campaign.balance += msg.value;
-        campaign.numApprovers += isNewApprover && contributor.isApprover ? 1 : 0;
+        contributor.isApprover = contributor.contribution >= minContributionForApprover;
+        numApprovers += isNewApprover && contributor.isApprover ? 1 : 0;
     }
 
-    function createCampaignRequest(uint campaignId, address payable recipient, string memory description, uint amount) validCampaign(campaignId) public {
-        Campaign storage campaign = campaigns[campaignId];
-        require(campaign.manager == msg.sender);
-        require(campaign.balance >= amount);
+    function createCampaignRequest(address payable recipient, string memory description, uint amount) admin public {
+        require(address(this).balance >= amount);
 
-        campaign.requests.push(CampaignRequest({
+        requests.push(CampaignRequest({
             recipient: recipient,
             description: description,
             amount: 0,
@@ -87,41 +79,33 @@ contract Crowdfund {
             isCompleted: false
         }));
 
-        emit CampaignRequestIdEvent(campaign.numRequests);
-        campaign.numRequests++;
+        emit RequestIdEvent(numRequests);
+        numRequests++;
     }
 
-    function approveCampaignRequest(uint campaignId, uint requestId) validRequest(campaignId, requestId) public {
-        Campaign storage campaign = campaigns[campaignId];
-        Contributor storage member = campaign.contributors[msg.sender];
-        require(member.isApprover);
+    function approveCampaignRequest(uint requestId) validRequest(requestId) public {
+        Contributor storage contributor = contributors[msg.sender];
+        require(contributor.isApprover);
 
-        CampaignRequest storage campaignRequest = campaign.requests[requestId];
+        CampaignRequest storage campaignRequest = requests[requestId];
         campaignRequest.numApprovals++;
-        campaignRequest.isApproved = campaignRequest.numApprovals > campaign.numApprovers/2;
+        campaignRequest.isApproved = campaignRequest.numApprovals > numApprovers/2;
     }
 
-    function completeCampaignRequest(uint campaignId, uint requestId) validRequest(campaignId, requestId) public {
-        Campaign storage campaign = campaigns[campaignId];
-        CampaignRequest storage campaignRequest = campaign.requests[requestId];
+    function completeCampaignRequest(uint requestId) validRequest(requestId) admin public {
+        CampaignRequest storage campaignRequest = requests[requestId];
         require(campaignRequest.isApproved);
-        require(campaign.balance >= campaignRequest.amount);
 
         campaignRequest.recipient.transfer(campaignRequest.amount);
-        campaign.balance -= campaignRequest.amount;
         campaignRequest.isCompleted = true;
     }
 
-    function getCampaignContributor(uint campaignId) validCampaign(campaignId) public view returns(Contributor memory) {
-        return campaigns[campaignId].contributors[msg.sender];
+    function getContributor() public view returns(Contributor memory) {
+        return contributors[msg.sender];
     }
 
-    function getCampaignBalance(uint campaignId) validCampaign(campaignId) public view returns(uint) {
-        return campaigns[campaignId].balance;
-    }
-
-    function getCampaignRequest(uint campaignId, uint requestId) validRequest(campaignId, requestId) public view returns(CampaignRequest memory) {
-        return campaigns[campaignId].requests[requestId];
+    function getCampaignRequests() public view returns(CampaignRequest[] memory) {
+        return requests;
     }
 
 }
