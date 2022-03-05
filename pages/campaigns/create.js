6adc6279fcc0c4f1952a4e7fc580449651841cef
Osamudiamen <osas.azamegbe@gmail.com>
2022-03-05 19:45:38 +0000

    error messages

--- pages/campaigns/create.js
+++ pages/campaigns/create.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import Layout from "../../components/Layout";
-import { Button, Form, Input } from "semantic-ui-react";
+import { Button, Form, Input, Message } from "semantic-ui-react";
 import CrowdfundWrapper from "../../crowdfund-solidity/crowdfund";
 
 
 const Create = () => {
     const [minimumContribution, SetMinimumContribution] = useState("");
     const [target, SetTarget] = useState("");
+    const [errorMessage, SetErrorMessage] = useState("");
+
+    const displayError = (error) => {
+        SetErrorMessage(error);
+        setTimeout(() => {
+            SetErrorMessage("");
+        }, 10000);
+    }
 
     // handlers
     const minimumContributionHandler = (event) => {
@@ -20,45 +28,50 @@ const Create = () => {
     const createCampaignHandler = async (event) => {
         event.preventDefault();
         if (target === '0') {
-            alert('Your campaign goal should not be zero :(');
+            displayError('Your campaign goal should not be zero :(');
             return;
         }
         if (target < minimumContribution) {
-            alert('The minimum contribution to your campaign should not be greater than your target!');
+            displayError('The minimum contribution to your campaign should not be greater than your target!');
             return;
         }
 
-        const crowdfund = new CrowdfundWrapper();
-        await crowdfund.createCampaign(minimumContribution, target);
-        SetTarget("");
-        SetMinimumContribution("");
-        alert('Campaign was created successfully!');
+        try {
+            const crowdfund = new CrowdfundWrapper();
+            await crowdfund.createCampaign(minimumContribution, target);
+            SetTarget("");
+            SetMinimumContribution("");
+            alert('Campaign was created successfully!');
+        } catch (error) {
+            displayError(error.message);
+        }
     }
 
     return (
         <Layout>
-            <Form onSubmit={createCampaignHandler}>
+            <Form onSubmit={createCampaignHandler} error={!!errorMessage}>
+                <Message error header="Ouch! An error occured" content={errorMessage} />
                 <Form.Field>
                     <label>Minimum contribution</label>
-                    <Input 
-                        type="number" 
+                    <Input
+                        type="number"
                         step="0.000001"
-                        placeholder="minimum contribution" 
-                        label="ETH" labelPosition="right" 
+                        placeholder="minimum contribution"
+                        label="ETH" labelPosition="right"
                         required
-                        value={minimumContribution} 
+                        value={minimumContribution}
                         onChange={minimumContributionHandler}
                     />
                 </Form.Field>
                 <Form.Field>
                     <label>Fund target</label>
-                    <Input 
+                    <Input
                         type="number"
                         step="0.000001"
-                        placeholder="target" 
-                        label="ETH" 
-                        labelPosition="right" 
-                        required 
+                        placeholder="target"
+                        label="ETH"
+                        labelPosition="right"
+                        required
                         value={target}
                         onChange={targetHandler}
                     />
