30c97181840b1c9f9ca3b5b191d8807c55b69f2c
Osamudiamen <osas.azamegbe@gmail.com>
2022-03-05 15:46:08 +0000

    display campaigns on cards

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import CrowdfundWrapper from '../crowdfund-solidity/crowdfund';
+import { Card } from 'semantic-ui-react';
 
 const Index = (props) => {
 
     return (
         <div>
+            <link
+                async
+                rel="stylesheet"
+                href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+            />
             <h1>Home page. WAGMI!!!</h1>
-            <p>{props.campaigns}</p>
+            <Card.Group>
+                {props.campaigns.map(address => {
+                    return <Card
+                        header={address}
+                        description={<a>View Campaign</a>}
+                        fluid={true}
+                    />
+                })}
+            </Card.Group>
         </div>
 
     );
