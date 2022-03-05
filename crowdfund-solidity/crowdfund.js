ed1c25b08a3b1f9bfb81efedfb3c401a161c627c
Osamudiamen <osas.azamegbe@gmail.com>
2022-03-05 19:26:43 +0000

    add logic to create campaign from form

--- crowdfund-solidity/crowdfund.js
+++ crowdfund-solidity/crowdfund.js
@@ -5,7 +5,7 @@ import compiled from "./build/Crowdfund.json";
 class CrowdfundWrapper {
     m_crowdfund;
     m_address;
-    
+
     constructor() {
         this.m_address = process.env.NEXT_PUBLIC_CROWDFUND_CONTRACT_ADDRESS;
         this.m_crowdfund = new web3.eth.Contract(compiled.Crowdfund.abi, this.m_address);
@@ -13,6 +13,13 @@ class CrowdfundWrapper {
 
     async getCampaigns() {
         return await this.m_crowdfund.methods.getCampaigns().call();
+    }
+
+    async createCampaign(mincontribution, target) {
+        mincontribution = web3.utils.toWei(mincontribution, 'ether');
+        target = web3.utils.toWei(target, 'ether');
+        const accounts = await web3.eth.getAccounts();
+        return await this.m_crowdfund.methods.createCampaign(mincontribution, target).send({ from: accounts[0] });
     }
 }
 
--- pages/campaigns/create.js
+++ pages/campaigns/create.js
@@ -1,19 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../../components/Layout";
-import { Button, Form } from "semantic-ui-react";
+import { Button, Form, Input } from "semantic-ui-react";
+import CrowdfundWrapper from "../../crowdfund-solidity/crowdfund";
 
 
 const Create = () => {
+    const [minimumContribution, SetMinimumContribution] = useState("");
+    const [target, SetTarget] = useState("");
+
+    // handlers
+    const minimumContributionHandler = (event) => {
+        SetMinimumContribution(event.target.value);
+    }
+
+    const targetHandler = (event) => {
+        SetTarget(event.target.value);
+    }
+
+    const createCampaignHandler = async (event) => {
+        event.preventDefault();
+        if (target === '0') {
+            alert('Your campaign goal should not be zero :(');
+            return;
+        }
+        if (target < minimumContribution) {
+            alert('The minimum contribution to your campaign should not be greater than your target!');
+            return;
+        }
+
+        const crowdfund = new CrowdfundWrapper();
+        await crowdfund.createCampaign(minimumContribution, target);
+        SetTarget("");
+        SetMinimumContribution("");
+        alert('Campaign was created successfully!');
+    }
+
     return (
         <Layout>
-            <Form>
+            <Form onSubmit={createCampaignHandler}>
                 <Form.Field>
                     <label>Minimum contribution</label>
-                    <input placeholder="minimum contribution" />
+                    <Input 
+                        type="number" 
+                        step="0.000001"
+                        placeholder="minimum contribution" 
+                        label="ETH" labelPosition="right" 
+                        required
+                        value={minimumContribution} 
+                        onChange={minimumContributionHandler}
+                    />
                 </Form.Field>
                 <Form.Field>
                     <label>Fund target</label>
-                    <input placeholder="target" />
+                    <Input 
+                        type="number"
+                        step="0.000001"
+                        placeholder="target" 
+                        label="ETH" 
+                        labelPosition="right" 
+                        required 
+                        value={target}
+                        onChange={targetHandler}
+                    />
                 </Form.Field>
                 <Button primary>Create</Button>
             </Form>
